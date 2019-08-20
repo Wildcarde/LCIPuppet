34b2060b9a370424de7e4367778843569c0e41d0
Garrett McGrath <gmcgrath815@gmail.com>
2019-08-19 23:23:25 -0400

    quick test moving node default to top to make sure all nodes get that setting

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,12 @@
 # Main Puppet File
 
+node default {
+  class { 'helloworld': }
+  class { 'helloworld::motd': }
+}
+
 node 'Compute-0', 'Compute-1', 'Storage-0', 'Storage-1', 'Storage-2',
 'Storage-3' {
 
         class { 'test':}
-}
-
-node default {
-  class { 'helloworld': }
-  class { 'helloworld::motd': }
 }
